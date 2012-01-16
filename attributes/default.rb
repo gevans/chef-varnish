c1f5f63a25dba24cdba76bd64d1b9c34c20138d4
Gabriel Evans <gabriel@codeconcoction.com>
2012-01-15 22:12:43 -0800

    Add configurable attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,21 @@ when "debian","ubuntu"
   set[:varnish][:dir]     = "/etc/varnish"
   set[:varnish][:default] = "/etc/default/varnish"
 end
+
+default[:varnish][:nfiles] = '131072'
+
+default[:varnish][:instance] = Chef::Config[:node_name]
+
+default[:varnish][:listen][:host] = '0.0.0.0'
+default[:varnish][:listen][:port] = '6081'
+
+default[:varnish][:admin][:host] = 'localhost'
+default[:varnish][:admin][:port] = '6082'
+
+default[:varnish][:backend][:host] = 'localhost'
+default[:varnish][:backend][:port] = '8080'
+
+default[:varnish][:user] = 'varnish'
+default[:varnish][:group] = 'varnish'
+
+default[:varnish][:storage] = "file,/var/lib/varnish/#{Chef::Config[:node_name]}/varnish_storage.bin,1G"
--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,66 @@
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
-description       "Instsalls and configures varnish"
-version           "0.8"
+description       "Installs and configures varnish"
+version           "0.9"
 
 recipe "varnish", "Installs and configures varnish"
 
 %w{ubuntu debian}.each do |os|
   supports os
 end
+
+attribute "varnish/nfiles",
+  :display_name => "Varnish Open File Descriptors Limit",
+  :description => "Maximum number of open files (for ulimit -n)",
+  :default => "131072"
+
+attribute "varnish/instance",
+  :display_name => "Varnish Instance Name",
+  :description => "Default varnish instance name is the local nodename",
+  :default => "current node name"
+
+attribute "varnish/listen/host",
+  :display_name => "Varnish Listening Address",
+  :description => "Host address that Varnish listens for connections on",
+  :default => "0.0.0.0"
+
+attribute "varnish/listen/port",
+  :display_name => "Varnish Listening Port",
+  :description => "Port that Varnish listens for connections on",
+  :default => "6081"
+
+attribute "varnish/admin/host",
+  :display_name => "Varnish Administration Host Address",
+  :description => "Host address that Varnish admin interface listens on",
+  :default => "localhost"
+
+attribute "varnish/admin/port",
+  :display_name => "Varnish Administration Port",
+  :description => "Port that Varnish admin interface listens on",
+  :default => "6082"
+
+attribute "varnish/backend/host",
+  :display_name => "Varnish Backend Host",
+  :description => "Backend address of content server that Varnish serves content from",
+  :default => "localhost"
+
+attribute "varnish/backend/port",
+  :display_name => "Varnish",
+  :description => "Backend port of content server that Varnish serves content from",
+  :default => "8080"
+
+attribute "varnish/user",
+  :display_name => "Varnish User",
+  :description => "User Varnish runs as",
+  :default => "varnish"
+
+attribute "varnish/group",
+  :display_name => "Varnish Group",
+  :description => "Group Varnish runs as",
+  :default => "varnish"
+
+attribute "varnish/storage",
+  :display_name => "Varnish Storage Options",
+  :description => "Storage type parameters that specify how Varnish caches objects",
+  :default => "file,/var/lib/varnish/$NODE_NAME/varnish_storage.bin,1G"
